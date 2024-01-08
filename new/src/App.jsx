5874a42ab28490bd1cfc95596e6458ef1ff13d57
bhagirathauti <bhagirath961778@gmail.com>
2024-01-08 11:35:25 +0530

    .

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Class from './class'
+import Class from './Class'
 
 function App() {
   const [count, setCount] = useState(0)
